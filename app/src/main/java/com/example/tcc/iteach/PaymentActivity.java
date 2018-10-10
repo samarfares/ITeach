b0999d62ca55ba7bf9f89c27a5729cb4bfbf6791
samarFares <samarFares@github.com>
2018-10-10 11:43:53 +0300

    payment

---
+++ app/src/main/java/com/example/tcc/iteach/PaymentActivity.java
@@ -0,0 +1,122 @@
+package com.example.tcc.iteach;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.wallet.AutoResolveHelper;
+import com.google.android.gms.wallet.CardRequirements;
+import com.google.android.gms.wallet.IsReadyToPayRequest;
+import com.google.android.gms.wallet.PaymentData;
+import com.google.android.gms.wallet.PaymentDataRequest;
+import com.google.android.gms.wallet.PaymentMethodTokenizationParameters;
+import com.google.android.gms.wallet.PaymentsClient;
+import com.google.android.gms.wallet.TransactionInfo;
+import com.google.android.gms.wallet.Wallet;
+import com.google.android.gms.wallet.WalletConstants;
+
+import java.util.Arrays;
+
+
+public class PaymentActivity extends AppCompatActivity {
+
+    private PaymentsClient mPaymentsClient;
+    private static final int LOAD_PAYMENT_DATA_REQUEST_CODE=888;
+    Button b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_payment );
+
+        mPaymentsClient = Wallet.getPaymentsClient(
+                this,
+                new Wallet.WalletOptions.Builder()
+                        .setEnvironment( WalletConstants.ENVIRONMENT_TEST)
+                        .build());
+
+        b= findViewById(R.id.buy_button);
+        b  .setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        PaymentDataRequest request = createPaymentDataRequest();
+                        if (request != null) {
+                            AutoResolveHelper.resolveTask(
+                                    mPaymentsClient.loadPaymentData(request), PaymentActivity.this,
+                                    // LOAD_PAYMENT_DATA_REQUEST_CODE is a constant value
+                                    // you define.
+                                    LOAD_PAYMENT_DATA_REQUEST_CODE);
+                        }
+                    }
+                });
+    }
+
+    private PaymentDataRequest createPaymentDataRequest() {
+        PaymentDataRequest.Builder request =
+                PaymentDataRequest.newBuilder()
+                        .setTransactionInfo(
+                                TransactionInfo.newBuilder()
+                                        .setTotalPriceStatus(WalletConstants.TOTAL_PRICE_STATUS_FINAL)
+                                        .setTotalPrice("10.00")
+                                        .setCurrencyCode("USD")
+                                        .build())
+                        .addAllowedPaymentMethod(WalletConstants.PAYMENT_METHOD_CARD)
+                        .addAllowedPaymentMethod(WalletConstants.PAYMENT_METHOD_TOKENIZED_CARD)
+                        .setCardRequirements(
+                                CardRequirements.newBuilder()
+                                        .addAllowedCardNetworks(
+                                                Arrays.asList(
+                                                        WalletConstants.CARD_NETWORK_AMEX,
+                                                        WalletConstants.CARD_NETWORK_DISCOVER,
+                                                        WalletConstants.CARD_NETWORK_VISA,
+                                                        WalletConstants.CARD_NETWORK_MASTERCARD))
+                                        .build());
+
+        PaymentMethodTokenizationParameters params =
+                PaymentMethodTokenizationParameters.newBuilder()
+                        .setPaymentMethodTokenizationType(
+                                WalletConstants.PAYMENT_METHOD_TOKENIZATION_TYPE_PAYMENT_GATEWAY)
+                        .addParameter("gateway", "example")
+                        .addParameter("gatewayMerchantId", "exampleGatewayMerchantId")
+                        .build();
+
+        request.setPaymentMethodTokenizationParameters(params);
+        return request.build();
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case LOAD_PAYMENT_DATA_REQUEST_CODE:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        PaymentData paymentData = PaymentData.getFromIntent(data);
+                        String token = paymentData.getPaymentMethodToken().getToken();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        break;
+                    case AutoResolveHelper.RESULT_ERROR:
+                        Status status = AutoResolveHelper.getStatusFromIntent(data);
+                        // Log the status for debugging.
+                        // Generally, there is no need to show an error to
+                        // the user as the Google Pay API will do that.
+                        break;
+                    default:
+                        // Do nothing.
+                }
+                break;
+            default:
+                // Do nothing.
+        }
+    }
+
+}
