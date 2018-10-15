4c907acf9bb1259878ac8c9be6404021e2f457ac
samarFares <samarFares@github.com>
2018-10-15 18:19:27 +0300

    ..

--- app/src/main/java/com/example/tcc/iteach/notifications2.java
+++ app/src/main/java/com/example/tcc/iteach/notifications2.java
@@ -2,6 +2,7 @@ package com.example.tcc.iteach;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -13,16 +14,78 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class notifications2 extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private DatabaseReference databaseReference;
 
+    ListView listView;
+    List<Message> list;
+    NotificationAdapter myAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        listView=(ListView) findViewById( R.id.list1 );
+        list = new ArrayList<>();
+        databaseReference = FirebaseDatabase.getInstance().getReference( "messages" );
+        databaseReference.addValueEventListener( new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snap : dataSnapshot.getChildren()) {
+
+                    Message message = snap.getValue( Message.class );
+                    list.add( message );
+
+                }
+
+
+
+                myAdapter = new NotificationAdapter( notifications2.this, R.layout.notificationitems, list );
+                listView.setAdapter( myAdapter );
+                Utility.setListViewHeightBasedOnChildren( listView );
+
+
+
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        } );
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(notifications2.this,ViewInstructorProfile.class);
+                intent.putExtra("email",list.get(position).getEmail());
+
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
